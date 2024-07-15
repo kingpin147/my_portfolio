8001d45b91d6add16503ad30cfaea6deafe6bcf4
kingpin147 <nomiking0072012@gmail.com>
2024-07-15 12:13:09 +0500

    service page completed

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,64 +1,62 @@
-'use client'
+'use client';
 
-import {BsArrowDownRight} from 'react-icons/bs'
+import { BsArrowDownRight } from 'react-icons/bs';
 import Link from "next/link";
-import { MotionConfig } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const services = [
   {
-    num:'01',
+    num: '01',
     title: 'Web Development',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi accusantium magni iste dolore, aspernatur neque nesciunt.',
-    href:'',
+    description: 'Specializing in creating robust and scalable web applications that cater to your business needs.',
+    href: '',
   },
   {
-    num:'01',
+    num: '02',
     title: 'Web Design',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi accusantium magni iste dolore, aspernatur neque nesciunt.',
-    href:'',
+    description: 'Crafting visually appealing and user-friendly websites that ensure a seamless user experience.',
+    href: '',
   },
   {
-    num:'02',
+    num: '03',
     title: 'SEO Specialist',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi accusantium magni iste dolore, aspernatur neque nesciunt.',
-    href:'',
+    description: 'Optimizing your website to rank higher on search engines and drive organic traffic to your business.',
+    href: '',
   },
   {
-    num:'03',
+    num: '04',
     title: 'Dev OPS',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi accusantium magni iste dolore, aspernatur neque nesciunt.',
-    href:'',
+    description: 'Implementing efficient DevOps practices to streamline development and deployment processes.',
+    href: '',
   },
-]
-
-import { motion } from 'framer-motion';
-
+];
 
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-        <div className="container mx-auto">
-          <motion.div initial={{opacity: 0}} animate:{{opacity: 1, transition:{delay:2.4, duration: 0.4, ease: "easeIn"},}}>
-          {services.map((service, index)=>{
-            return <div key={index}>
-              <div>
-                <div>{service.num}</div>
-                <Link href={service.href}>
-                <BsArrowDownRight/>
+      <div className="container mx-auto">
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }}}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => (
+            <div key={index} className="flex flex-col gap-6 justify-center group">
+              <div className='w-full flex justify-between items-center'>
+                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                <Link href={service.href} className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45 '>
+                  <BsArrowDownRight className="text-primary text-3xl"/>
                 </Link>
-                <h2>{service.title}</h2>
-                <p>{service.description}</p>
-                <div className="norder-b border-white/20 w-full"></div>
               </div>
+              <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>{service.title}</h2>
+              <p className='text-white/60'>{service.description}</p>
+              <div className="border-b border-white/20 w-full"></div>
             </div>
-          })}
-          
-          </motion.div>
-
-          </div>
+          ))}
+        </motion.div>
+      </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React from "react";
 import CountUp from "react-countup";
 
@@ -30,7 +30,7 @@ const Stats = () => {
             return (
               <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
                 <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
-                <p className={'${item.text.length<15? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80'}>{item.text}</p>
+                <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
               </div>
             );
           })}
