330972d53bcd1dbc2dfd438fe691aacce49971a7
Muhammad Nouman Attique <nomiking0072012@gmail.com>
2024-07-18 00:09:25 +0500

    resume working

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,14 +1,13 @@
-"use client"
+"use client";
 
-import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs,FaDocker, FaPython,} from "react-icons/fa"
-import {SiTailwindcss, SiNextdotjs, SiApachekafka,SiKong, SiTypescript,SiFastapi,SiPoetry } from "react-icons/si"
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, FaDocker, FaPython } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiApachekafka, SiKong, SiTypescript, SiFastapi, SiPoetry } from "react-icons/si";
 
 const about = {
-  icon:"",
+  icon: "",
   title: "About me",
-  description:
-  "Lorem17",
-  info:[
+  description: "A dedicated and skilled freelancer with expertise in web development, web design, SEO, and DevOps.",
+  info: [
     {
       fieldName: "Name",
       fieldValue: "Muhammad Nouman Attique",
@@ -42,163 +41,228 @@ const about = {
       fieldValue: "English, Urdu, Punjabi",
     },
     {
-      fieldName: "Programming Languanges",
-      fieldValue: "HTML, CSS, JavaScript, TypeScript, ReactJS, NextJS, Python,Docker Compose, Kafka",
+      fieldName: "Programming Languages",
+      fieldValue: "HTML, CSS, JavaScript, TypeScript, ReactJS, NextJS, Python, Docker Compose, Kafka",
     },
-  ]
+  ],
 };
 
 const experience = {
-  icon:"",
+  icon: "",
   title: "My Experience",
-  description:
-  "Lorem17",
-  items:[
+  description: "With over 2 years of hands-on experience in various roles, I have honed my skills in web development, teaching, and system administration.",
+  items: [
     {
-      Company: "District & Session Court",
-      Position: "Case Management System (CMS) Admin",
-      Duraton: "2022 - Present"
+      company: "District & Session Court",
+      position: "Case Management System (CMS) Admin",
+      duration: "2022 - Present",
     },
     {
-      Company: "Code Crush Company (remote/freelance)",
-      Position: "Wix Velo JavaScript Developer",
-      Duraton: "2023 - Present"
+      company: "Code Crush Company (remote/freelance)",
+      position: "Wix Velo JavaScript Developer",
+      duration: "2023 - Present",
     },
     {
-      Company: "NGO (Part Time)",
-      Position: "Computer Science Teacher",
-      Duraton: "2018 - 2023"
+      company: "NGO (Part Time)",
+      position: "Computer Science Teacher",
+      duration: "2018 - 2023",
     },
-   ]
-  };
+  ],
+};
 
-  const education = {
-    icon:"",
-    title: "My Educaion",
-    description:
-    "Lorem17",
-    items:[
-      {
-        Institute: "Presidential Initiative for Artificial Intelligence & Computing (PIAIC) ",
-        Degree: "Certified Cloud Applied Generative AI Engineer (GenEng)",
-        Duration: "2023 - present"
-      },
-      {
-        Institute: "Minhaj University Lahore",
-        Degree: "M.Phil Computer Science",
-        Duration: "2021 - 2023"
-      },
-      {
-        Institute: "Virtual University of Pakistan",
-        Degree: "Master In Computer Science",
-        Duration: "2018 - 2020"
-      },
-      
-     ]
-    };
+const education = {
+  icon: "",
+  title: "My Education",
+  description: "My educational background includes advanced degrees and certifications in computer science and AI, equipping me with a strong foundation in technology.",
+  items: [
+    {
+      institute: "Presidential Initiative for Artificial Intelligence & Computing (PIAIC)",
+      degree: "Certified Cloud Applied Generative AI Engineer (GenEng)",
+      duration: "2023 - present",
+    },
+    {
+      institute: "Minhaj University Lahore",
+      degree: "M.Phil Computer Science",
+      duration: "2021 - 2023",
+    },
+    {
+      institute: "Virtual University of Pakistan",
+      degree: "Master In Computer Science",
+      duration: "2018 - 2020",
+    },
+  ],
+};
 
+const skills = {
+  icon: "",
+  title: "My Skills",
+  description: "I possess a diverse set of skills in web technologies, programming languages, and development tools.",
+  skillList: [
+    {
+      Icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      Icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      Icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      Icon: <FaReact />,
+      name: "React",
+    },
+    {
+      Icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      Icon: <FaNodeJs />,
+      name: "Node.js",
+    },
+    {
+      Icon: <FaDocker />,
+      name: "Docker",
+    },
+    {
+      Icon: <FaPython />,
+      name: "Python",
+    },
+    {
+      Icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      Icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+    {
+      Icon: <SiApachekafka />,
+      name: "Apache Kafka",
+    },
+    {
+      Icon: <SiKong />,
+      name: "Kong",
+    },
+    {
+      Icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      Icon: <SiFastapi />,
+      name: "FastAPI",
+    },
+    {
+      Icon: <SiPoetry />,
+      name: "Poetry",
+    },
+  ],
+};
 
-    const skills = {
-      icon: "",
-      title: "My Skills",
-      description: "Lorem17",
-      skillList: [
-        {
-          Icon: <FaHtml5 />,
-          name: "HTML 5",
-        },
-        {
-          Icon: <FaCss3 />,
-          name: "CSS 3",
-        },
-        {
-          Icon: <FaJs />,
-          name: "JavaScript",
-        },
-        {
-          Icon: <FaReact />,
-          name: "React",
-        },
-        {
-          Icon: <FaFigma />,
-          name: "Figma",
-        },
-        {
-          Icon: <FaNodeJs />,
-          name: "Node.js",
-        },
-        {
-          Icon: <FaDocker />,
-          name: "Docker",
-        },
-        {
-          Icon: <FaPython />,
-          name: "Python",
-        },
-        {
-          Icon: <SiTailwindcss />,
-          name: "Tailwind CSS",
-        },
-        {
-          Icon: <SiNextdotjs />,
-          name: "Next.js",
-        },
-        {
-          Icon: <SiApachekafka />,
-          name: "Apache Kafka",
-        },
-        {
-          Icon: <SiKong />,
-          name: "Kong",
-        },
-        {
-          Icon: <SiTypescript />,
-          name: "TypeScript",
-        },
-        {
-          Icon: <SiFastapi />,
-          name: "FastAPI",
-        },
-        {
-          Icon: <SiPoetry />,
-          name: "Poetry",
-        },
-      ],
-    };
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {Tootltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import {motion} from "framer-motion";
-
-const resume = () => {
+const Resume = () => {
   return (
     <motion.div
-    initial={{opacity: 0}}
-    animate={{
-      opacity:1,
-      transition: {delay:2.4, duration:0.4, ease: "easeIn"},
-    }}
-    className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <Tabs 
-          defaultValue="experience"
-          className="flex flex-col xl:flex-row gap-[60px]"
-        >
-          <TabsList 
-            className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6"
-          >
-          <TabsTrigger value="experience">Experience</TabsTrigger>
-          <TabsTrigger value="education">Education</TabsTrigger>
-          <TabsTrigger value="skills"> Skills</TabsTrigger>
-          <TabsTrigger value="about">About me</TabsTrigger>
+        <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills"> Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold ">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-col-2 gap-[30px]">
+                    {experience.items.map((item, index) => (
+                      <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 ">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold ">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-col-2 gap-[30px]">
+                    {education.items.map((item, index) => (
+                      <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 ">
+                        <span className="text-accent">{item.institute}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.duration}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="skills" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold ">{skills.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                <ScrollArea>
+                  <ul>
+                    {skills.skillList.map((skill, index) => (
+                      <li key={index}>
+                        <span>{skill.Icon}</span>
+                        <h3>{skill.name}</h3>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="about" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold ">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                <ul>
+                  {about.info.map((info, index) => (
+                    <li key={index}>
+                      <h3>{info.fieldName}</h3>
+                      <p>{info.fieldValue}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
         </Tabs>
       </div>
+    </motion.div>
+  );
+};
 
-      </motion.div>
-  )
-}
-
-export default resume
+export default Resume;
